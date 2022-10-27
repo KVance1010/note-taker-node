99ce1f915866a53446df519eea8dcbebd3c4be96
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-10-26 23:48:38 -0700

    finished post note

--- server.js
+++ server.js
@@ -1,54 +1,59 @@
 const express = require('express');
 const fileSystem = require('fs');
 const filePath = require('path');
+const dataBase = require('./db/db.json');
+let i = 1;
 
 const app = express();
-const PORT = 3001;
+const PORT = 4200;
 
-app.use(express.static('./public'));
 app.use(express.json());
+app.use(express.static('./public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(filePath.join(__dirname, '/public/index.html'))
-);
+app.get('/', (req, res) => {
+	res.sendFile(filePath.join(__dirname, '/public/index.html'));
+});
 
 app.get('/notes', (req, res) => {
-    res.sendFile(filePath.join(__dirname, '/public/notes.html'))   
+	res.sendFile(filePath.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+	res.json(dataBase);
 });
 
 app.post('/api/notes', (req, res) => {
-    
-    
+	const { title, text } = req.body;
+
+	if (title && text) {
+		const newNote = {
+			title,
+			text,
+			id: Math.floor(Math.random() * 0x10000 + 1)
+				.toString(16)
+				.substring(1),
+		};
+		fileSystem.readFile('./db/db.json', 'utf8', (error, notes) => {
+			if (error) {
+				console.error(error);
+			} else {
+				let parsedNotes = JSON.parse(notes);
+				parsedNotes.push(newNote);
+				dataBase = parsedNotes;
+				fileSystem.writeFile(
+					'./db/db.json',
+					JSON.stringify(parsedNotes, null, '/t'),
+					(error) => {
+						error
+							? console.error(error)
+							: console.info('Successfully updated notes!');
+					}
+				);
+			}
+		});
+	}
 });
 
-
-// fileSystem.readFile('./db/db.json', 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       // Convert string into JSON object
-//       const parsedReviews = JSON.parse(data);
-
-//       // Add a new review
-//       parsedReviews.push(newReview);
-//       reviews = parsedReviews;
-//       // Write updated reviews back to the file
-//       //Note: The second and third arguments provided by the JSON.stringify() 
-//       //method are optional arguments that help with formatting. 
-//       //The first is a replacer() function, which we set to null, 
-//       //and the third is the space argument that adds indentation, whitespace, 
-//       //and line break characters to make it easier to read.
-//       fs.writeFile(
-//         './db/db.json',
-//         JSON.stringify(parsedReviews, null, 4),
-//         (writeErr) =>
-//           writeErr
-//             ? console.error(writeErr)
-//             : console.info('Successfully updated reviews!')
-//       );
-//     }
-//   });
-
-  app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.listen(PORT, () => {
+	console.log(`App listening at http://localhost:${PORT} 🚀`);
+});
